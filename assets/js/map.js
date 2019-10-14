0440c0a07ab0fc3f3f9496ee681b1b2ee4d034a0
Alexandra Kapp <akapp@MAC17.local>
2019-10-14 13:49:05 +0200

    fix map hover and responsiveness

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,53 +1,60 @@
-let config,
-map,
-projection,
-path,
-width,
-height,
-hasBase = false,
-dataLayer = false,
-navContent,
-navMap,
-state = {
-	content:0,
-	map:0
-},
-lang = 0,
-tooltip_map;
-
-d3.json("config.json")
-.then((_config) => {
-	config = _config;
+document.addEventListener('DOMContentLoaded',function() {
 	
-	// TODO: get dynamic width
-	width = config.positions.map.width;
-	height = config.positions.map.height;
 	
-	// Setup Map
+	const style = window.getComputedStyle(document.getElementById("intro"), null);
+	const padding_total = parseFloat(style['paddingLeft'].replace('px', '')) * 2;
+	const width_temp = parseFloat(style['width']);
+	const width = width_temp - padding_total;
+	const height = width * 0.7;
 	
-	projection = d3.geoMercator()
-	.center([13.41, 52.51])
-	.scale(config.positions.map.scale)
-	.translate([width/2, height/2]);
 	
-	path = d3.geoPath()
-	.projection(projection);
+	let config,
+	map,
+	projection,
+	path,
+	dataLayer = false,
+	navContent,
+	navMap,
+	state = {
+		content:0,
+		map:0
+	},
+	lang = 0,
+	tooltip_map;
 	
-	map = d3.select("#map")
-	.append("svg")
-	.attr("id", "map-svg")
-	.attr("width", width)
-	.attr("height", height);
-	
-	dataLayer = map.append("g")
-	.attr("id", "dataLayer");
-	
-	//Setup Tooltip
-	navContent = d3.select("#main-navigation")
-	.selectAll("li")
-		.data(config.content)
-		.enter()
-		.append("li")
+	d3.json("config.json")
+	.then((_config) => {
+		config = _config;
+		
+		
+		
+		d3.json("data/xii.geojson")
+		.then(data => {
+			
+			// Setup Map
+			projection = d3.geoMercator()
+			.fitSize([width, height], data);
+			
+			path = d3.geoPath()
+			.projection(projection);
+			
+			map = d3.select("#map")
+			.append("svg")
+			.attr("id", "map-svg")
+			.attr("width", width)
+			.attr("height", height);
+			
+			dataLayer = map.append("g")
+			.attr("id", "dataLayer");
+			
+			
+			
+			//Setup Tooltip
+			navContent = d3.select("#main-navigation")
+			.selectAll("li")
+			.data(config.content)
+			.enter()
+			.append("li")
 			.text((d) => d.nav[lang])
 			.on("click", (d, i) => {
 				updateNav(i, 0);
@@ -55,166 +62,155 @@ d3.json("config.json")
 				// console.log('click')
 			});
 			
-	tooltip_map = d3.select("#map-wrapper").append('div')
-	.attr('class', 'tooltip')
-	.style('display', 'none');
-	
-	
-	// Setup Navigation
-	updateNav(0,0);
-	
-	// Setup Legend Bar
-	var scale = d3.select('#map')
-	.append('svg')
-	.attr('id', 'scale-svg')
-	.attr('width', (width / 2) + 100)
-	.attr('height', 60);
-	
-	// Create the svg:defs element and the main gradient definition.
-	var svgDefs = scale.append('defs');
-	var mainGradient = svgDefs.append('linearGradient')
-	.attr('id', 'mainGradient');
-	
-	// Create the stops of the main gradient. Each stop will be assigned
-	// a class to style the stop using CSS.
-	mainGradient.append('stop')
-	.attr('class', 'stop-left')
-	.attr('stop-color', config.colors[0])
-	.attr('offset', '0');
-	
-	mainGradient.append('stop')
-	.attr('class', 'stop-right')
-	.attr('stop-color', config.colors[1])
-	.attr('offset', '1');
-	
-	// Use the gradient to set the shape fill, via CSS.
-	
-	scale.append('rect')
-	.classed('filled', true)
-	.attr('x', 50)
-	.attr('width', width / 2)
-	.attr('height', 30);
-	
-	scale.append('text')
-	.attr('x', 10)
-	.attr('y', 25)
-	.attr('fill', 'grey')
-	.text('0 %');
-	
-	scale.append('text')
-	.attr('x', (width / 2) + 60)
-	.attr('y', 25)
-	.attr('fill', 'grey')
-	.text('40 %');
-	
-	scale.append('text')
-	.attr('x', 80)
-	.attr('y', 50)
-	.attr('fill', 'grey')
-	.text('% Grundsicherungsempfänger*innen');
-	
-	
-})
-.catch((err) => {
-	console.log(err);
-});
-
-const updateNav = (contentState, mapState) => {
-	state.content = contentState;
-	state.map = mapState;
-	
-	navContent
-	.classed("active", (d,i) => (i===state.content) ? true : false);
-
-	d3.selectAll("#sub-navigation *").remove();
-	
-	navMap = d3.select("#sub-navigation")
-	.selectAll("li")
-	.data(config.content[state.content].maps)
-	.enter()
-	.append("li")
-	.text((d) => d.nav[lang])
-	.classed("active", (d,i) => (i===state.map) ? true : false)
-	.on("click", (d, i) => {
-		updateNav(state.content, i);
+			tooltip_map = d3.select("#map-wrapper").append('div')
+			.attr('class', 'tooltip')
+			.style('display', 'none');
+			
+			
+			// Setup Navigation
+			updateNav(0,0);
+			
+			// Setup Legend Bar
+			var scale = d3.select('#map')
+			.append('svg')
+			.attr('id', 'scale-svg')
+			.attr('width', (width / 2) + 100)
+			.attr('height', 60);
+			
+			// Create the svg:defs element and the main gradient definition.
+			var svgDefs = scale.append('defs');
+			var mainGradient = svgDefs.append('linearGradient')
+			.attr('id', 'mainGradient');
+			
+			// Create the stops of the main gradient. Each stop will be assigned
+			// a class to style the stop using CSS.
+			mainGradient.append('stop')
+			.attr('class', 'stop-left')
+			.attr('stop-color', config.colors[0])
+			.attr('offset', '0');
+			
+			mainGradient.append('stop')
+			.attr('class', 'stop-right')
+			.attr('stop-color', config.colors[1])
+			.attr('offset', '1');
+			
+			// Use the gradient to set the shape fill, via CSS.
+			
+			scale.append('rect')
+			.classed('filled', true)
+			.attr('x', 50)
+			.attr('width', width / 2)
+			.attr('height', 30);
+			
+			scale.append('text')
+			.attr('x', 10)
+			.attr('y', 25)
+			.attr('fill', 'grey')
+			.text('0 %');
+			
+			scale.append('text')
+			.attr('x', (width / 2) + 60)
+			.attr('y', 25)
+			.attr('fill', 'grey')
+			.text('40 %');
+			
+			scale.append('text')
+			.attr('x', 80)
+			.attr('y', 50)
+			.attr('fill', 'grey')
+			.text('% Grundsicherungsempfänger*innen');
+			
+		})
+	})
+	.catch((err) => {
+		console.log(err);
 	});
 	
-	makeMap(state.content, state.map);
-}
-
-const switchLang = (inputLang) => {
-	lang = inputLang;
-	
-	d3.selectAll("#lang-navigation li")
-	.classed("active", (d) => (lang == d.value) ? true : false);
-	
-	navContent.text((d) => d.nav[lang]);
-	navMap.text((d) => d.nav[lang]);
-	
-	makeMap(state.content, state.map);
-};
-
-
-
-const makeMap = (contentId, mapId) => {
-	const mapConfig = config.content[contentId].maps[mapId];
-	
-	// Set Texts
-	d3.select("#content-topic").html(config.content[contentId].title[lang]);
-	d3.select("#content-title").html(mapConfig.title[lang]);
-	d3.select("#content-description").html(mapConfig.description[lang]);
-	
-	dataLayer.selectAll("*").remove();
-	
-	// Make Map
-	
-	d3.json("data/" + mapConfig.data)
-	.then(data => {
+	const updateNav = (contentState, mapState) => {
+		state.content = contentState;
+		state.map = mapState;
 		
-		// make sure the attribute column is a number
-		data.features.forEach((d) => {
-			d.properties[mapConfig.attribute] = parseFloat(d.properties[mapConfig.attribute]);
+		navContent
+		.classed("active", (d,i) => (i===state.content) ? true : false);
+		
+		d3.selectAll("#sub-navigation *").remove();
+		
+		navMap = d3.select("#sub-navigation")
+		.selectAll("li")
+		.data(config.content[state.content].maps)
+		.enter()
+		.append("li")
+		.text((d) => d.nav[lang])
+		.classed("active", (d,i) => (i===state.map) ? true : false)
+		.on("click", (d, i) => {
+			updateNav(state.content, i);
 		});
 		
-		const scale = d3.scaleLinear()
-		.domain(("extent" in mapConfig) ? mapConfig.extent : [0, 40]) //d3.extent(data.features, (d) => d.properties[mapConfig.attribute]
-		.range(("colors" in mapConfig) ? mapConfig.colors : ["#98FB98", "#FF0000"]);
+		makeMap(state.content, state.map);
+	}
+	
+	
+	
+	const makeMap = (contentId, mapId) => {
+		const mapConfig = config.content[contentId].maps[mapId];
 		
-		// tooltips
-		function mouseover(){
-			tooltip_map.style('display', 'block')
-			.style('position', 'absolute');
-		}
-		function mousemove(){
-			var d = d3.select(this).data()[0]
-			var selected_map = config.content[0].maps[state.map].attribute
-			tooltip_map
-			.html('<h3 class="tooltip--title">' + d.properties.Name + '</h3>' + 
-			'<div class="tooltip--datawrapper"> <div class="tooltip--datawrapper--row">' +
-			'<p class="attr">%</p>' +
-			'<p class="value">' + d.properties[selected_map] + '</p>' +
-			'</div> </div>')
-			.style('left', (d3.event.pageX + 20) + 'px')
-			.style('top', (d3.event.pageY + 20) + 'px');
-		}
+		// Set Texts
+		d3.select("#content-topic").html(config.content[contentId].title[lang]);
+		d3.select("#content-title").html(mapConfig.title[lang]);
+		d3.select("#content-description").html(mapConfig.description[lang]);
 		
-		function mouseout(){
-			tooltip_map.style('display', 'none');
-		}
+		dataLayer.selectAll("*").remove();
 		
-		dataLayer.selectAll("path")
-		.data(data.features)
-		.enter()
-		.append("path")
-		.attr("d", path)
-		.on('mouseover', mouseover)
-		.on('mousemove', mousemove)
-		.on('mouseout', mouseout)
-		.attr("fill", (d) => {return isNaN(d.properties[mapConfig.attribute]) ? '#383838' :
-		scale(d.properties[mapConfig.attribute]);
-	});
-})
-.catch((err) => {
-	console.log(err);
-})
+		// Make Map
+		
+		d3.json("data/" + mapConfig.data)
+		.then(data => {
+			
+			// make sure the attribute column is a number
+			data.features.forEach((d) => {
+				d.properties[mapConfig.attribute] = parseFloat(d.properties[mapConfig.attribute]);
+			});
+			
+			const scale = d3.scaleLinear()
+			.domain(("extent" in mapConfig) ? mapConfig.extent : [0, 40]) //d3.extent(data.features, (d) => d.properties[mapConfig.attribute]
+			.range(("colors" in mapConfig) ? mapConfig.colors : ["#98FB98", "#FF0000"]);
+			
+			// tooltips
+			function mouseover(){
+				tooltip_map.style('display', 'block')
+				.style('position', 'absolute');
+			}
+			function mousemove(){
+				var d = d3.select(this).data()[0]
+				var selected_map = config.content[0].maps[state.map].attribute
+				tooltip_map
+				.html('<h3 class="tooltip--title">' + d.properties.Name + '</h3>' + 
+				'<div class="tooltip--datawrapper"> <div class="tooltip--datawrapper--row">' +
+				'<p class="attr">%</p>' +
+				'<p class="value">' + d.properties[mapConfig.attribute] + '</p>' +
+				'</div> </div>')
+				.style('left', (d3.event.pageX + 20) + 'px')
+				.style('top', (d3.event.pageY + 20) + 'px');
+			}
+			
+			function mouseout(){
+				tooltip_map.style('display', 'none');
+			}
+			
+			dataLayer.selectAll("path")
+			.data(data.features)
+			.enter()
+			.append("path")
+			.attr("d", path)
+			.on('mouseover', mouseover)
+			.on('mousemove', mousemove)
+			.on('mouseout', mouseout)
+			.attr("fill", (d) => {return isNaN(d.properties[mapConfig.attribute]) ? '#383838' :
+			scale(d.properties[mapConfig.attribute]);
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	})
 }
+})
--- assets/js/timelapse.js
+++ assets/js/timelapse.js
@@ -1,191 +1,202 @@
-let timelapse_map,
-dataLayer_tl,
-projection_timelapse, 
-path_timelapse,
-width_tl,
-height_tl,
-tooltip_tl,
-year_label;
-
-var min = 0;
-var max = 40;
-
-d3.json("config.json")
-.then((_config) => {
-    config = _config;
+document.addEventListener('DOMContentLoaded',function() {
     
-    width_tl = config.positions.map.width;
-    height_tl = config.positions.map.height;
+    const style = window.getComputedStyle(document.getElementById("intro"), null);
+    const padding_total = parseFloat(style['paddingLeft'].replace('px', '')) * 2;
+    const width_temp = parseFloat(style['width']);
+    const width = width_temp - padding_total;
+    var height = width * 0.7;
     
-    projection_timelapse = d3.geoMercator()
-    .center([13.41, 52.51])
-    .scale(config.positions.map.scale)
-    .translate([width / 2, height / 2]);
+    let timelapse_map,
+    dataLayer_tl,
+    projection_timelapse, 
+    path_timelapse,
+    tooltip_tl,
+    year_label;
     
-    path_timelapse = d3.geoPath()
-    .projection(projection_timelapse);
+    var min = 0;
+    var max = 40;
     
-    timelapse_map = d3.select("#timelapse-map")
-    .append("svg")
-    .attr("width", width_tl)
-    .attr("height", height_tl);
-    
-    dataLayer_tl = timelapse_map.append("g")
-    .attr("id", "dataLayer_tl");
-    
-    //Setup year label
-    year_label = d3.select("#year-label").append('text')
-    .attr('transform','translate('+(0)+','+12+')')
-    .text('Jahr');
-
-    // Setup Legend Bar
-    var scale = d3.select('#timelapse-map')
-    .append('svg')
-    .attr('id', 'scale-svg')
-    .attr('width', (width / 2) + 100)
-    .attr('height', 60);
-    
-    // Create the svg:defs element and the main gradient definition.
-    var svgDefs = scale.append('defs');
-    var mainGradient = svgDefs.append('linearGradient')
-    .attr('id', 'mainGradient');
-    
-    // Create the stops of the main gradient. Each stop will be assigned
-    // a class to style the stop using CSS.
-    mainGradient.append('stop')
-    .attr('class', 'stop-left')
-    .attr('stop-color', config.colors[0])
-    .attr('offset', '0');
-    
-    mainGradient.append('stop')
-    .attr('class', 'stop-right')
-    .attr('stop-color', config.colors[1])
-    .attr('offset', '1');
-    
-    // Use the gradient to set the shape fill, via CSS.
-    
-    scale.append('rect')
-    .classed('filled', true)
-    .attr('x', 50)
-    .attr('width', width / 2)
-    .attr('height', 30);
-    
-    scale.append('text')
-    .attr('x', 10)
-    .attr('y', 25)
-    .attr('fill', 'grey')
-    .text(min + " %");
-    
-    scale.append('text')
-    .attr('x', (width / 2) + 60)
-    .attr('y', 25)
-    .attr('fill', 'grey')
-    .text(max + " %");
-    
-    scale.append('text')
-    .attr('x', 80)
-    .attr('y', 50)
-    .attr('fill', 'grey')
-    .text('% Grundsicherungsempfänger*innen');
-    
-    
-    //Setup Tooltip
-    var tooltip_tl = d3.select("#timelapse-map-wrapper").append("div")
-    .attr('class', 'tooltip')
-    .style('display', 'none');
-    
-    update(2006)
-    
-    function update(year
-        ) {
-            d3.csv("data/timelapse.csv")
-            .then(function (data) {
-                
-                min = Infinity;
-                max = 0;
-                
-                d3.json("data/lor_planungsraeume.geojson")
-                .then(json => {
-                    
-                    var min = Infinity;
-                    var max = 0;
-                    
-                    data.forEach(function (d) {
-                        
-                        var region = d.Kennung;
-                        var value = +d['y' + [year
-                        ]];
-                        
-                        if (value < min)
-                        min = value
-                        
-                        if (value > max)
-                        max = value
-                        
-                        json.features.forEach(function (e) {
-                            var name = e.properties.spatial_name
-                            if (name == region) {
-                                e.properties.value = value;
-                            }
-                        })
-                    })
-                    
-                    var color = d3.scaleLinear()
-                    .range([config.colors[0], config.colors[1]])
-                    .domain([0, 40]);
-                    
-                    // update map
-                    
-                    dataLayer_tl.selectAll("path").remove();
-                    
-                    
-                    dataLayer_tl.selectAll("path")
-                    .data(json.features)
-                    .enter()
-                    .append("path")
-                    .attr("fill", function (d) { return color(d.properties.value); })
-                    .attr("d", path_timelapse)
-                    .on('mouseover', mouseover)
-                    .on('mousemove', mousemove)
-                    .on('mouseout', mouseout)
-                    
-                    // tooltips
-                    function mouseover() {
-                        tooltip_tl.style('display', 'block')
-                        .style('position', 'absolute');
-                    }
-                    function mousemove() {
-                        var d = d3.select(this).data()[0]
-                        var mouse = d3.mouse(timelapse_map.node()).map(function (d) {
-                            return parseInt(d);
-                        });
-                        tooltip_tl
-                        .attr('style', 'left:' + (mouse[0] + 15) + 'px; top:' + (mouse[1] - 35) + 'px')
-                        .html('<h3 class="tooltip--title">' + d.properties.spatial_alias + '</h3>' +
-                        '<div class="tooltip--datawrapper"> <div class="tooltip--datawrapper--row">' +
-                        '<p class="attr">%</p>' +
-                        '<p class="value">' + d.properties.value + '</p>' +
-                        '</div> </div>')
-                        .style('left', (d3.event.pageX + 20) + 'px')
-                        .style('top', (d3.event.pageY + 20) + 'px');
-                    }
-                    
-                    function mouseout() {
-                        tooltip_tl.style('display', 'none');
-                    }
-                    
-                    year_label
-                    .text('Jahr : ' + year);
-                })
-            })
-            .catch(function (error) {
-                console.log(error)
-            })
-        }
+    d3.json("config.json")
+    .then((_config) => {
+        config = _config;
         
-        d3.select("#slider")
-        .on("input", function (d) { update(+this.value) })
+        d3.json("data/xii.geojson")
+        .then(data => {
+            
+            projection_timelapse = d3.geoMercator()
+            .fitSize([width, height], data);
+            
+            path_timelapse = d3.geoPath()
+            .projection(projection_timelapse);
+            
+            
+            
+        })
+        
+        timelapse_map = d3.select("#timelapse-map")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+        
+        dataLayer_tl = timelapse_map.append("g")
+        .attr("id", "dataLayer_tl");
+        
+        //Setup year label
+        year_label = d3.select("#year-label").append('text')
+        .attr('transform','translate('+(0)+','+12+')')
+        .text('Jahr');
+        
+        // Setup Legend Bar
+        var scale = d3.select('#timelapse-map')
+        .append('svg')
+        .attr('id', 'scale-svg')
+        .attr('width', (width / 2) + 100)
+        .attr('height', 60);
+        
+        // Create the svg:defs element and the main gradient definition.
+        var svgDefs = scale.append('defs');
+        var mainGradient = svgDefs.append('linearGradient')
+        .attr('id', 'mainGradient');
+        
+        // Create the stops of the main gradient. Each stop will be assigned
+        // a class to style the stop using CSS.
+        mainGradient.append('stop')
+        .attr('class', 'stop-left')
+        .attr('stop-color', config.colors[0])
+        .attr('offset', '0');
+        
+        mainGradient.append('stop')
+        .attr('class', 'stop-right')
+        .attr('stop-color', config.colors[1])
+        .attr('offset', '1');
+        
+        // Use the gradient to set the shape fill, via CSS.
+        
+        scale.append('rect')
+        .classed('filled', true)
+        .attr('x', 50)
+        .attr('width', width / 2)
+        .attr('height', 30);
+        
+        scale.append('text')
+        .attr('x', 10)
+        .attr('y', 25)
+        .attr('fill', 'grey')
+        .text(min + " %");
+        
+        scale.append('text')
+        .attr('x', (width / 2) + 60)
+        .attr('y', 25)
+        .attr('fill', 'grey')
+        .text(max + " %");
+        
+        scale.append('text')
+        .attr('x', 80)
+        .attr('y', 50)
+        .attr('fill', 'grey')
+        .text('% Grundsicherungsempfänger*innen');
+        
+        //Setup Tooltip
+        tooltip_tl = d3.select("#timelapse-map-wrapper").append("div")
+        .attr('class', 'tooltip')
+        .style('display', 'none');
+        
+        update(2006)
+        
+        function update(year
+            ) {
+                d3.csv("data/timelapse.csv")
+                .then(function (data) {
+                    
+                    min = Infinity;
+                    max = 0;
+                    
+                    d3.json("data/lor_planungsraeume.geojson")
+                    .then(json => {
+                        
+                        var min = Infinity;
+                        var max = 0;
+                        
+                        data.forEach(function (d) {
+                            
+                            var region = d.Kennung;
+                            var value = +d['y' + [year
+                            ]];
+                            
+                            if (value < min)
+                            min = value
+                            
+                            if (value > max)
+                            max = value
+                            
+                            json.features.forEach(function (e) {
+                                var name = e.properties.spatial_name
+                                if (name == region) {
+                                    e.properties.value = value;
+                                }
+                            })
+                        })
+                        
+                        var color = d3.scaleLinear()
+                        .range([config.colors[0], config.colors[1]])
+                        .domain([0, 40]);
+                        
+                        // update map
+                        
+                        dataLayer_tl.selectAll("path").remove();
+                        
+                        
+                        dataLayer_tl.selectAll("path")
+                        .data(json.features)
+                        .enter()
+                        .append("path")
+                        .attr("fill", function (d) { return color(d.properties.value); })
+                        .attr("d", path_timelapse)
+                        .on('mouseover', mouseover)
+                        .on('mousemove', mousemove)
+                        .on('mouseout', mouseout)
+                        
+                        // tooltips
+                        function mouseover() {
+                            tooltip_tl.style('display', 'block')
+                            .style('position', 'absolute');
+                        }
+                        function mousemove() {
+                            var d = d3.select(this).data()[0]
+                            var mouse = d3.mouse(timelapse_map.node()).map(function (d) {
+                                return parseInt(d);
+                            });
+                            tooltip_tl
+                            .attr('style', 'left:' + (mouse[0] + 15) + 'px; top:' + (mouse[1] - 35) + 'px')
+                            .html('<h3 class="tooltip--title">' + d.properties.spatial_alias + '</h3>' +
+                            '<div class="tooltip--datawrapper"> <div class="tooltip--datawrapper--row">' +
+                            '<p class="attr">%</p>' +
+                            '<p class="value">' + d.properties.value + '</p>' +
+                            '</div> </div>')
+                            .style('left', (d3.event.pageX + 20) + 'px')
+                            .style('top', (d3.event.pageY + 20) + 'px');
+                        }
+                        
+                        function mouseout() {
+                            tooltip_tl.style('display', 'none');
+                        }
+                        
+                        year_label
+                        .text('Jahr : ' + year);
+                    })
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
+            }
+            
+            d3.select("#slider")
+            .on("input", function () { update(+this.value) })
+            
+            
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        
     })
-    .catch((err) => {
-        console.log(err);
-    });
